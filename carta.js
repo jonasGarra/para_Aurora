7a44c1177754d5fd0af7ad131bbc88bde153c01f
jonasGarra <jonasriket@gmail.com>
2026-02-17 10:36:47 +0100

    Arreglo cartas

--- carta.js
+++ carta.js
@@ -1,6 +1,20 @@
 // Función para abrir el sobre al hacer clic
 function abrirSobre(elemento) {
-  // Añade la clase 'open' al contenedor del sobre
-  // elemento.classList.add("open");
+  // 1. Alternamos la clase 'open' en el sobre
   elemento.classList.toggle("open");
+
+  // 2. TRUCO PARA EL Z-INDEX (Sube el padre por encima de todo)
+  // Buscamos el padre .carta-wrapper
+  const padre = elemento.closest(".carta-wrapper");
+
+  if (elemento.classList.contains("open")) {
+    // Si acabamos de abrir, ponemos z-index ALTO
+    padre.style.zIndex = "9999";
+  } else {
+    // Si acabamos de cerrar, esperamos un poco (a que baje el papel)
+    // y reseteamos el z-index
+    setTimeout(() => {
+      padre.style.zIndex = "10";
+    }, 6000); // 600ms = tiempo de la animación
+  }
 }
