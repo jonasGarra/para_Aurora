393de80638b8f0dbdb7bfa6bff88a3a0e8b7729d
jonasGarra <jonasriket@gmail.com>
2026-02-17 10:13:33 +0100

    Cartas

---
+++ carta.js
@@ -0,0 +1,6 @@
+// Función para abrir el sobre al hacer clic
+function abrirSobre(elemento) {
+  // Añade la clase 'open' al contenedor del sobre
+  // elemento.classList.add("open");
+  elemento.classList.toggle("open");
+}
